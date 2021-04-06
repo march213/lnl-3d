2eba00645d8813d06e422bb3aa238a07b8e56208
Eugenia Molodetskaya <jane.molodetskaya@gmail.com>
2021-04-06 11:02:26 -0400

    added materials

--- src/App.js
+++ src/App.js
@@ -1,57 +1,112 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import './App.css'
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      <h1>
+        Lunch n Learn:
+        <br />
+        Three.JS and Shaders intro
+      </h1>
+      <section>
+        <h2>Inspiration:</h2>
+        <ul>
+          <li>
+            <a href='https://threejs-journey.xyz/' target='_blank' rel='noopener noreferrer'>
+              The ultimate Three.js course
+            </a>
+          </li>
+          <li>
+            <a href='https://bruno-simon.com/' target='_blank' rel='noopener noreferrer'>
+              bruno-simon
+            </a>
+          </li>
+          <li>
+            <a href='https://zen.ly/' target='_blank' rel='noopener noreferrer'>
+              zen.ly
+            </a>
+          </li>
+          <li>
+            <a href='https://lusion.co/' target='_blank' rel='noopener noreferrer'>
+              lusion.co
+            </a>
+          </li>
+          <li>
+            <a href='https://covidpinata.ooo/' target='_blank' rel='noopener noreferrer'>
+              covidpinata
+            </a>
+          </li>
+          <li>
+            <a href='https://jonathanalpmyr.com/' target='_blank' rel='noopener noreferrer'>
+              jonathanalpmyr
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Resources:</h2>
+        <ul>
+          <li>
+            <a
+              href='https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              threejs docs
+            </a>
+          </li>
+          <li>
+            <a href='https://threejs.org/' target='_blank' rel='noopener noreferrer'>
+              threejs
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://threejsfundamentals.org/threejs/lessons/threejs-fundamentals.html'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              threejs-fundamentals
+            </a>
+          </li>
+          <li>
+            <a href='https://thebookofshaders.com/' target='_blank' rel='noopener noreferrer'>
+              thebookofshaders
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://github.com/terkelg/awesome-creative-coding/blob/master/readme.md#shaders--opengl--webgl'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              shaders--opengl--webgl
+            </a>
+          </li>
+          <li>
+            <a href='https://frontendmasters.com/courses/webgl-shaders/' target='_blank' rel='noopener noreferrer'>
+              Advanced Creative Coding with WebGL & Shaders
+            </a>
+          </li>
+          <li>
+            <a href='https://discoverthreejs.com/' target='_blank' rel='noopener noreferrer'>
+              discoverthreejs
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Example:</h2>
+        <iframe
+          src='https://codesandbox.io/embed/threejs-shaders-lunch-and-learn-6u5vx?fontsize=14&hidenavigation=1&theme=dark'
+          style={{ width: '100%', height: '640px', border: 0, borderRadius: '4px', overflow: 'hidden' }}
+          title='threejs-shaders-lunch-and-learn'
+          allow='accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking'
+          sandbox='allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts'
+        ></iframe>
+      </section>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
